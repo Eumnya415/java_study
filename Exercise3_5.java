78602b7f5f7711472727ebd55a48a1f7c0679b06
eumnya <2124524@naver.com>
2023-09-26 18:49:43 +0900

    자바 200제 : 연습문제 3-5

---
+++ Exercise3_5.java
@@ -0,0 +1,8 @@
+package com.study.run.Exercise_0926;
+
+public class Exercise3_5 {
+    public static void main(String[] args) {
+        int num = 333;
+        System.out.println(num/10*10+1);
+    }
+}
