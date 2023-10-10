ca43440df512b63271ca6262d738b18430b92cb1
eumnya <2124524@naver.com>
2023-10-10 19:59:26 +0900

    사칙연산 계산기(1)

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,24 @@
 package ex01;
 
 public class Calculator {
-    //정사각형의 넓이
-    double areaRectangle(double width) {
-        return width * width;
+    public double add(double num1, double num2) {
+        return num1 + num2;  // 덧셈 메소드
     }
 
-    //직사각형의 넓이
-    double areaRectangle(double width, double height) {
-        return width * height;
+    public double subtract(double num1, double num2) {
+        return num1 - num2;  // 뺄셈 메소드
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;  // 곱셈 메소드
+    }
+
+    public double divide(double num1, double num2) {  // 나눗셈 메소드
+        if (num2 != 0) {  // 0으로 나눌 경우 처리
+            return num1 / num2;
+        } else {
+            System.out.println("0으로 나눌 수 없습니다.");
+            return Double.NaN; // Not-a-Number를 반환하여 오류 표시
+        }
     }
 }
