7d14b51df2d063918e7b99cb26303c40c575812c
eumnya <2124524@naver.com>
2023-10-10 19:51:35 +0900

    정적 멤버 사용(1) : 정적 필드 pi, 정적 메소드 plus, minus

---
+++ Static.java
@@ -0,0 +1,13 @@
+package ex01;
+
+public class Static {
+    static double pi = 3.14159;
+
+    static int plus(int x, int y) {
+        return x + y;
+    }
+
+    static int minus(int x, int y) {
+        return x - y;
+    }
+}
